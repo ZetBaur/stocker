a51d4fbc5f3c54c135481d5b73812a2f1f08f91f
Your Name <you@example.com>
2021-01-03 11:58:36 +0600

    start

---
+++ #src/js/app.js
@@ -0,0 +1,10 @@
+
+
+
+    //@prepros-append files/functions.js
+    //@prepros-append files/regular.js
+    //@prepros-append files/sliders.js
+    //@prepros-append files/script.js
+    //@prepros-append files/forms.js
+    //@prepros-append files/scroll.js
+
---
+++ #src/js/vendors.js
@@ -0,0 +1,21 @@
+
+
+
+    //@prepros-append libs/smoothScroll.js
+    //@prepros-append libs/inputmask.min.js
+    //@prepros-append libs/swiper.min.js
+    //@prepros-append libs/nouislider.js
+    //@prepros-append libs/wNumb.min.js
+    //@prepros-append libs/lightgallery.min.js
+    //@prepros-append libs/tippy.js
+    //@prepros-append libs/datepicker-full.min.js
+
+
+
+
+
+    
+
+    
+
+
